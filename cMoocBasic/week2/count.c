e735208d8139669aae4f2e63dc96b3135a5db7cd
TorrieL <hmminicity2@163.com>
2020-04-12 18:01:07 +0800

    scanf

--- cMoocBasic/week2/count.c
+++ cMoocBasic/week2/count.c
@@ -2,11 +2,13 @@
 
 int main()
 {
+  int amount = 100;
   int price = 0;
   printf("please input price(¥)");
   scanf("%d", &price);
-
-  int change = 100 - price;
+  printf("please input cash(¥)");
+  scanf("%d", &amount);
+  int change = amount - price;
   printf("%d¥ back to you\n", change);
   
   return 0;
