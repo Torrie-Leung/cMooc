6376aa9a59d14be6e27e78eb2c4df27101c85839
TorrieL <hmminicity2@163.com>
2020-04-17 23:10:25 +0800

    average with float

--- cs50/L2-Arrays/scores.c
+++ cs50/L2-Arrays/scores.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
+// get average func
+float average(int length, int array[])
+{
+  int sum = 0;
+  for(int i = 0; i < length; i++)
+  {
+    sum += array[i];
+  }
+  return (float)sum/(float)length;
+}
 int main(void)
 {
   
@@ -7,7 +17,8 @@ int main(void)
   int scores[n];
   for(int i = 0; i < n; i++)
   {
-    scores[i] = get_int("Score %i: ", i+1);
-    printf("Average: todo\n");
+    scores[i] = get_int("Score %i: ", i);
+   
   }
+   printf("Average: %f\n",average(n,scores));
 }
