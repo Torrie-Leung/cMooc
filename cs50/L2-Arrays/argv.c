bc4ce7fc94c87c655d7a820491d36f48b05588eb
TorrieL <hmminicity2@163.com>
2020-04-18 21:15:36 +0800

    argv

---
+++ cs50/L2-Arrays/argv.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+  if(argc ==2)
+  {
+    printf("hey, %s\n", argv[1]);
+  }
+  else
+  {
+    printf("hey,dummies\n");
+  }
+  
+}
