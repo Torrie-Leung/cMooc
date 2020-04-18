8fdd271a6a335c3ca81cf1a1541ccffe5de01583
TorrieL <hmminicity2@163.com>
2020-04-18 19:55:23 +0800

    str length

---
+++ cs50/L2-Arrays/str.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+int main(void)
+{
+  string s = get_string("Input: ");
+  printf("Output: ");
+  int n = strlen(s);
+
+  for(int i = 0; s[i] != '\0'; i++)
+  {
+    printf("%c", s[i]);
+  }
+  printf("\n");
+  // better condition
+  for(int i = 0; i < n; i++)
+  {
+    printf("%c", s[i]);
+  }
+  printf("\n");
+}
