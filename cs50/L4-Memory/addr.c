d9edd790c3d34a0e13ce842be289825aafb99450
TorrieL <hmminicity2@163.com>
2020-04-24 13:24:42 +0800

    pointer

--- cs50/L4-Memory/addr.c
+++ cs50/L4-Memory/addr.c
@@ -4,6 +4,12 @@ int main(void)
 {
   int n = 50;
   int *p = &n;
+  char *s = "CHIMP";
+  printf("%s\n", s);
+  printf("%p\n", s);
+  printf("%p\n", &s[0]);
+  printf("%p\n", &s[1]);
+  printf("%p\n", &s[2]);
   printf("hexademal addr/pointer of n: %p\n", p);
   printf("grab the value from pointer: %i\n", *p);
 }
