881fa6a1763d07d73624732a9ddf7d00a3324300
TorrieL <hmminicity2@163.com>
2020-04-27 22:50:02 +0800

    deep copy in c

--- cs50/copy.c
+++ cs50/copy.c
@@ -1,12 +1,24 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-  string s = get_string("s: ");
-  string t = s;
+  // string s = get_string("s: ");
+  // string t = s;
 
+  // t[0] = toupper(t[0]);
+  // printf("%s\n", s);
+  // printf("%s\n", t);
+  char *s = get_string("s: ");
+  char *t = malloc(strlen(s) + 1);
+  int n = strlen(s);
+  for(int i = 0; i < n + 1; i++)
+  {
+    t[i] = s[i];
+  }
   t[0] = toupper(t[0]);
   printf("%s\n", s);
   printf("%s\n", t);
