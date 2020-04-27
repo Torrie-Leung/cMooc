1f53645fe233bd82f6ba9cd6ce2bcb83b0016790
TorrieL <hmminicity2@163.com>
2020-04-27 20:59:59 +0800

    shallow copy in c

---
+++ cs50/copy.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+  string s = get_string("s: ");
+  string t = s;
+
+  t[0] = toupper(t[0]);
+  printf("%s\n", s);
+  printf("%s\n", t);
+}
