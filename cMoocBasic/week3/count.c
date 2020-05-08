19c3a0f2d4a54641c049395a11fc1f497ff2b4c8
TorrieL <hmminicity2@163.com>
2020-05-08 21:30:49 +0800

    basic contidional count

---
+++ cMoocBasic/week3/count.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  // initialize
+  int price = 0;
+  int bill = 0;
+  // input
+  printf("请输入总金额：");
+  scanf("%d", &price);
+  printf("请输入票面：");
+  scanf("%d", &bill);
+  //output
+  if(bill >= price){
+    printf("应该找您：%d\n", bill - price);
+  }else{
+    printf("请补足：%d元\n", price - bill);
+  }
+}
