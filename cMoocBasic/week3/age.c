b3028f797005526c048d4f74b3f7200f735db3f5
TorrieL <hmminicity2@163.com>
2020-05-10 10:48:50 +0800

    basic conditions

---
+++ cMoocBasic/week3/age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  const int MINIOR = 35;
+  int age = 0;
+  printf("请输入你的年龄：");
+  scanf("%d", &age);
+  printf("你的年龄是%d岁。\n", age);
+  if(age < MINIOR){
+    printf("年轻是美好的,加油！\n");
+  }else{
+    printf("年龄决定了你的精神世界，好好珍惜吧。\n");
+  }
+  
+}
